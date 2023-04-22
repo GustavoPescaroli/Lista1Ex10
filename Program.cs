f2d63d5c3e7f7e60a3eae9ef2b462fd54f8e4f33
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-04-21 21:57:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double real;
+            double dolar = 5.05;
+            double x;
+
+            Console.WriteLine("Valor em reais");
+            x = double.Parse(Console.ReadLine());
+
+            real = dolar * x;
+            Console.WriteLine("Reais {0}", real);
+            
+        }
+    }
+}
